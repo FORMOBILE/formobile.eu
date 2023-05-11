b1a5e604a44dd4a20a540c780ac7ccc4f397ed92
bibble <>
2023-05-11 12:47:33 +0200

    - renamed function and file

--- pictures/move_pictures_to_folder.py
+++ pictures/move_pictures_to_folder.py
@@ -8,10 +8,9 @@ import random
 from shutil import copy2
 
 
-def copy_pictures_to_folder(from_folder: str, to_folder: str, amount: int) -> None:
+def move_pictures_to_folder(from_folder: str, to_folder: str, amount: int) -> None:
     """
-    Copies a given amount of random pictures from from_folder to to_folder and deletes them from from_folder to avoid
-    duplicates.
+    Moves a given amount of random pictures from from_folder to to_folder to avoid duplicates.
 
     :param from_folder: path to folder where the picture dataset is stored
     :param to_folder: path to where the sample should be stored
@@ -26,6 +25,6 @@ def copy_pictures_to_folder(from_folder: str, to_folder: str, amount: int) -> No
         dst = os.path.join(to_folder, pic)
         # Copying
         copy2(src, dst)
-        logging.info(f'Copied {src} to {dst}')
+        logging.info(f'Moved {src} to {dst}')
         # Deleting
         os.remove(src)
