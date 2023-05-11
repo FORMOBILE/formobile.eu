016010d2157b2f8f073842e7c40d21c69198ba62
bibble <>
2023-05-11 12:46:48 +0200

    - fixed imports - path to pictures dataset is now set in definitions.py - fixed path references - fixed date/time format

--- pictures/main_pictures.py
+++ pictures/main_pictures.py
@@ -10,8 +10,8 @@ import sys
 from datetime import datetime
 from ppadb.client import Client
 from send_pictures import send_pictures
-from copy_pictures_to_folder import copy_pictures_to_folder
-from definitions import ROOT_DIR, CONFIG_PATH
+from move_pictures_to_folder import move_pictures_to_folder
+from definitions import ROOT_DIR, CONFIG_PATH, PICTURES_PATH
 from datasets.modify_timestamps import modify_timestamps
 
 
@@ -26,9 +26,8 @@ if len(devices) == 0:
 with open(CONFIG_PATH, 'r') as f:
     config = json.load(f)
 
-# The original data set is under '/home/alexander/Downloads/VG_100K'
 # Source path is a copy of the original data set, because original data set should not be modified
-src_path = os.path.join(ROOT_DIR, 'datasets', 'pictures', 'without_meta_copy')
+src_path = PICTURES_PATH
 # Path is the directory where the serial number folders are stored
 path = os.path.join(ROOT_DIR, 'datasets', 'pictures', 'gallery')
 # Important for the threading process later
@@ -40,7 +39,7 @@ for i, dev in enumerate(devices):
     for j in range(len(config['People'])):
 
         if config['People'][j]['Mobile devices'][0]['Serial number'] == device.serial:
-            full_path = path + device.serial
+            full_path = os.path.join(path, device.serial)
             os.chdir(path)
             if os.path.isdir(full_path):
                 # If folder exists, it deletes the content of it
@@ -52,20 +51,20 @@ for i, dev in enumerate(devices):
                 os.wait()
                 create.close()
             else:
-                # If folder does not exists, it creates one
+                # If folder does not exist, it creates one
                 create = os.popen(f'mkdir {device.serial}')
                 os.wait()
                 create.close()
 
             # Copies the right amount of pictures to the destination path on this PC
-            dst_path = path + device.serial + '/'
+            dst_path = os.path.join(path, device.serial)
             number_of_pics = config['People'][j]['Mobile devices'][0]['nr of pics']
-            copy_pictures_to_folder(src_path, dst_path, number_of_pics)
+            move_pictures_to_folder(src_path, dst_path, number_of_pics)
 
             # Modifies and randomizes the timestamps as given in the config.json
             dev = config['People'][j]['Mobile devices'][0]
-            start = datetime.strptime(dev['timeframe start'], '%Y-%m-%dT%H:%M:%S.%fZ')
-            end = datetime.strptime(dev['timeframe end'], '%Y-%m-%dT%H:%M:%S.%fZ')
+            start = datetime.strptime(dev['timeframe start'], '%Y-%m-%dT%H:%M:%S')
+            end = datetime.strptime(dev['timeframe end'], '%Y-%m-%dT%H:%M:%S')
             modify_timestamps(dst_path, start, end)
 
             # Stores the naming convention of each device defined in the config.json
