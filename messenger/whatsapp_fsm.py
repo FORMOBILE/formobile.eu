c968e81dd891f05726b31b960f7a80302315ca13
bibble <>
2023-05-15 15:05:35 +0200

    - fixed imports

--- messenger/whatsapp_fsm.py
+++ messenger/whatsapp_fsm.py
@@ -28,7 +28,7 @@ from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as ec, ui
-from messenger.whatsapp.whatsapp_setup import whatsapp_setup
+from messenger.whatsapp_setup import whatsapp_setup
 
 
 def prime(fun):
